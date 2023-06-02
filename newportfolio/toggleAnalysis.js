7a06585182d6f8c467901b61b31c81638acb7ee1
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-03 00:10:09 +0100

    Final correction to w-100 grid
    - Project cards barely have a gap

--- newportfolio/toggleAnalysis.js
+++ newportfolio/toggleAnalysis.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   //remove 'analysis-[color]... classes from elements'
   $(".analysis").each(function () {
     $(this).removeClass(
-      "analysis-Red analysis-Yellow analysis-Cyan analysis-Purple"
+      "analysis-Red analysis-Green analysis-Yellow analysis-Cyan analysis-Purple"
     );
   });
 });
