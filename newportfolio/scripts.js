3fe8e5c7b9935f3ccd9745275bc8bc12a071fa12
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-05 22:07:53 +0100

    S3

--- newportfolio/scripts.js
+++ newportfolio/scripts.js
@@ -1,12 +1,12 @@
 //TOGGLE ANALYSIS-------------------------
-$(document).ready(function () {
-  // remove 'analysis-[color]... classes from elements'
-  $(".analysis").each(function () {
-    $(this).removeClass(
-      "analysis-Red analysis-Green analysis-Yellow analysis-Cyan analysis-Purple"
-    );
-  });
-});
+// $(document).ready(function () {
+//   // remove 'analysis-[color]... classes from elements'
+//   $(".analysis").each(function () {
+//     $(this).removeClass(
+//       "analysis-Red analysis-Green analysis-Yellow analysis-Cyan analysis-Purple"
+//     );
+//   });
+// });
 
 //I TRIED BREAKPOINTS AND CLASS ADDITIONS-------------------------
 // let mqXL = window.matchMedia("(max-width: 1199px)");
