150b1879b19920506968fbbeb57121d9261a4119
tgoulder4 <118992509+tgoulder4@users.noreply.github.com>
2023-06-28 17:55:28 +0100

    split into frontend and backedn

--- newportfolio-backend/backend/server.js
+++ newportfolio-backend/backend/server.js
@@ -1,7 +1,10 @@
 const app = require("express")();
 const port = process.env.PORT || 3500;
 app.get(
-  "/"
+  "/",
+  (req, res) => {
+    res.send("hi!");
+  }
   //send fullstack one
 );
 app.get(
